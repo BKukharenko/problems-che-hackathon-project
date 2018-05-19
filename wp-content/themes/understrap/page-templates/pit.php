25d4bbe997be150fd3a800e1d21c03bc5aeff766
Yuliia Vdovenko <yuliiavdovenko@gmail.com>
2018-05-19 09:50:46 +0300

    Add page templates

---
+++ wp-content/themes/understrap/page-templates/pit.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template name: Pit
+ * The template for displaying pit page.
+ *
+ * @package understrap
+ */
+
+get_header();
+$container   = get_theme_mod( 'understrap_container_type' );?>
+
+
+<?php get_footer(); ?>
